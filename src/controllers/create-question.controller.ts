9a651c2653501b377f614564c0435b7796c78ba0
ïago alexandre <iagoaap16@gmail.com>
2024-12-10 05:39:54 -0300

    create question route

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,16 +1,52 @@
-import { Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { CurrentUser } from "src/auth/current-user-decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { UserPayload } from "src/auth/jwt.strategy";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(
+    private prismaService: PrismaService
+  ) {}
   
   @Post()
   async handle(
+    @Body(new ZodValidationPipe(createQuestionBodySchema)) 
+    body: CreateQuestionBodySchema,
+
     @CurrentUser() user: UserPayload
   ) {
-    console.log(user)
+    const { title, content } = body 
+
+    await this.prismaService.question.create({
+      data: {
+        title,
+        slug: this.convertTitleToSlug(title),
+        content,
+        authorId: user.sub
+      }
+    })
+  }
+
+  private convertTitleToSlug(title: string): string {
+    return title
+      .normalize('NFD') // Normalize the string to decompose combined characters
+      .replace(/[\u0300-\u036f]/g, '') // Remove diacritical marks
+      .toLowerCase() // Convert to lowercase
+      .replace(/[^a-z0-9 ]/g, '') // Remove invalid characters
+      .replace(/\s+/g, '-') // Replace spaces with hyphens
+      .replace(/-+/g, '-') // Replace multiple hyphens with a single hyphen
+      .replace(/^-|-$/g, ''); // Remove leading and trailing hyphens
   }
 }

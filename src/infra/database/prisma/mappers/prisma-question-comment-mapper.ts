5b200ba9e857e1782fbd17da0ddd64d5a3b968a0
ïago alexandre <iagoaap16@gmail.com>
2024-12-14 11:30:48 -0300

    creating attachments

---
+++ src/infra/database/prisma/mappers/prisma-question-comment-mapper.ts
@@ -0,0 +1,30 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
+import { Prisma, Comment as PrismaQuestionComment } from '@prisma/client';
+
+export class PrismaQuestionCommentMapper {
+  static toDomain(raw: PrismaQuestionComment): QuestionComment {
+    if(!raw.questionId) {
+      throw new Error('QuestionComment must have an questionId')
+    }
+
+    return QuestionComment.create({
+      content: raw.content,
+      questionId: new UniqueEntityID(raw.questionId),
+      authorId: new UniqueEntityID(raw.authorId),
+      createdAt: raw.createdAt,
+      updatedAt: raw.updatedAt,
+    }, new UniqueEntityID(raw.id))
+  }
+
+  static toPrisma(questionComment: QuestionComment): Prisma.CommentUncheckedCreateInput {
+    return {
+      id: questionComment.id.toString(),
+      authorId: questionComment.authorId.toString(),
+      questionId: questionComment.questionId.toString(),
+      content: questionComment.content,
+      createdAt: questionComment.createdAt,
+      updatedAt: questionComment.updatedAt,
+    }
+  }
+}
